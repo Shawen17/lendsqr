e4bbd307b9d8b8cc97bec937a4ea7658e6913571
shawen17 <shawen022@yahoo.com>
2024-02-26 17:11:02 -0500

    redirects added

--- src/action/auth.js
+++ src/action/auth.js
@@ -114,7 +114,7 @@ export const login = (email, password) => async (dispatch) => {
 
   try {
     const res = await axios.post(
-      `${process.env.REACT_APP_LENDSQR_API_URL}/auth/jwt/create/`,
+      `lendsqr-backend.vercel.app/auth/jwt/create/`,
       body,
       config
     );
